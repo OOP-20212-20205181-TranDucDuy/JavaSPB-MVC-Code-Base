d4fd18afbf3e2873bbe3c39ac5d14bf9109bbeba
OOP-20212-20205181-TranDucDuy <103793658+OOP-20212-20205181-TranDucDuy@users.noreply.github.com>
2024-03-18 21:33:12 +0700

    first commit

---
+++ src/main/java/com/example/javacodebase/JavaCodebaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacodebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodebaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javacodebase/JavaCodebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javacodebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
